9c7b15df6af4dac0c7eaba25d5058159157946ec
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-09-10 15:42:35 -0400

    added axios API call function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
-// import axios from "axios";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import axios from "axios";
 // import firebase from "./firebase"
 import "./App.css";
 
@@ -10,10 +10,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      events: [],
+      isLoading: true,
       apiKey: "RzVQVthdwCGvl8TaJVeNTb3nxVceKaFu",
       secret: "6RHq8MbsPw41lPHG"
     };
   }
+
+  getEvents = () => {
+    axios({
+      method: "GET",
+      url: "https://app.ticketmaster.com/discovery/v2/events",
+      dataResponse: "json",
+      params: {
+        format: "json",
+        apikey: "RzVQVthdwCGvl8TaJVeNTb3nxVceKaFu"
+      }
+    }).then(results => {
+      results = results.data.events;
+      this.setState({
+        events: results,
+        isLoading: false
+      });
+    });
+  };
+
+  componentDidMount () {
+    this.getEvents();
+  }
+
   render() {
     return (
       <div className="App">
