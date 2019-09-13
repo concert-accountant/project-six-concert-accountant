e008a0eb7ec11d1ffae7b2054538a736ae09e23b
Jeff Rudd <jeffrudd19@gmail.com>
2019-09-13 16:34:31 -0400

    Basic Firebase Functionality Added to Events.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,17 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCrKZI3FRGd5Fq3sMdkRufn8fUT3uspt7o",
-    authDomain: "concert-accountant.firebaseapp.com",
-    databaseURL: "https://concert-accountant.firebaseio.com",
-    projectId: "concert-accountant",
-    storageBucket: "",
-    messagingSenderId: "258247958616",
-    appId: "1:258247958616:web:59a46fe5ffd95b82dd2a2d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCKYr8akor5Dkq4r0owOzgmoq-bhIC-yr8",
+  authDomain: "concertaccountant.firebaseapp.com",
+  databaseURL: "https://concertaccountant.firebaseio.com",
+  projectId: "concertaccountant",
+  storageBucket: "",
+  messagingSenderId: "915408755528",
+  appId: "1:915408755528:web:c4ca3e4d61759e8861317d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   export default firebase;
