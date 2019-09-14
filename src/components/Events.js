9f4aaac3eb84336d28807472885a649974a2b385
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-09-14 00:53:48 -0400

    working on search function

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Search from './Search';
+import Search from "./Search";
 import axios from "axios";
 import firebase from "../firebase";
 
@@ -12,57 +12,61 @@ class Events extends Component {
       isLoading: true,
       apiKey: "RzVQVthdwCGvl8TaJVeNTb3nxVceKaFu",
       secret: "6RHq8MbsPw41lPHG",
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?",
+      baseUrl:
+        "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=RzVQVthdwCGvl8TaJVeNTb3nxVceKaFu&classificationName=music",
       location: "Toronto",
+      // query: "&keyword=",
       priceInfo: "",
       minPrice: 0,
       maxPrice: 1,
       test: [],
-      userInput: '',
+      userInput: "",
+      searchTerms: "",
+      noResults: ""
     };
   }
 
-  handleChange = (e) => {
-    this.setState({userInput: e.target.value})
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-    const dbRef = firebase.database().ref();
-    dbRef.push(this.state.userInput);
-    this.setState({
-      userInput: ""
-    })
-  }
-
-  removeTestItem(testItemId) {
-    const dbRef = firebase.database().ref();
-    dbRef.child(testItemId).remove();
-  }
-
-  getEvents = () => {
+  //axios call to get initial array of events based on city
+  getEvents = url => {
     axios({
       method: "GET",
-      url: "https://app.ticketmaster.com/discovery/v2/events.json?",
+      url: url,
       dataResponse: "json",
       params: {
         apikey: "RzVQVthdwCGvl8TaJVeNTb3nxVceKaFu",
         city: this.state.location,
         classificationName: "music"
-        // id: "1778vpG65T-dA3a"
       }
     }).then(results => {
       results = results.data._embedded.events;
-      // results.length = 10;
-      console.log("results", results);
-      this.setState({
-        events: results,
-        isLoading: false
-      });
+      if (results.length !== 0) {
+        // results.length = 10;
+        this.setState({
+          events: results,
+          isLoading: false
+        });
+      } else if (results.length === 0) {
+        this.setState({
+          noResult:
+            "Sorry, your search had no results! Make sure you put commas between search terms.",
+          isLoading: false,
+          events: []
+        });
+      } else {
+        this.setState({
+          events: results,
+          isLoading: false,
+          noResult: null
+        });
+      }
+      //call filterResults Function to filter through the array and only return desired items
       this.filterResults();
       console.log("new results array", this.state.filteredEvents);
     });
   };
 
+  //function to filter through the events array in state and take out events that don't have a priceRange and then again for items that have a priceRange but no min/max price in the array
   filterResults = () => {
     const resultsCopy = [...this.state.events];
     const priceResults = resultsCopy.filter(singleEvent => {
@@ -76,83 +80,146 @@ class Events extends Component {
     });
   };
 
-  componentDidMount() {
-    this.getEvents();
-    
+  //function to store search input data in search component
+  searchInput = event => {
+    this.setState({
+      searchTerms: event.target.value
+    });
+    console.log(this.state.searchTerms);
+  };
+
+  ///////////////////////////////////
+  /////////Working on it/////////////
+  //////////////////////////////////
+  //function to handle form submit and show results in search component
+  searchSubmit = e => {
+    e.preventDefault();
+    //add query into const and url between the two
+    const { baseUrl, searchTerms } = this.state;
+    this.setState(
+      () => {
+        return {
+          url: `${baseUrl}${searchTerms}`,
+          searchTerms: ""
+        };
+      },
+      () => {
+        console.log(this.state.url);
+        this.getEvents(this.state.url);
+      }
+    );
+  };
+
+  //Function to setstate for userInput when button is pressed
+  handleChange = e => {
+    this.setState({
+      userInput: e.target.value
+    });
+  };
+
+  //Function to push userInput data to Firebase
+  handleClick = e => {
+    e.preventDefault();
     const dbRef = firebase.database().ref();
-    dbRef.on('value', (response) => {
+    dbRef.push(this.state.userInput);
+    this.setState({
+      userInput: ""
+    });
+  };
+
+  //Function to remove items when remove button is clicked and store in firebase
+  removeTestItem(testItemId) {
+    const dbRef = firebase.database().ref();
+    dbRef.child(testItemId).remove();
+  }
+
+  //functions and data to run when component loads
+  componentDidMount() {
+    this.getEvents(this.state.url);
+
+    const dbRef = firebase.database().ref();
+    dbRef.on("value", response => {
       const newState = [];
       const data = response.val();
       for (let key in data) {
-        newState.push({key: key, name: data[key]});
+        newState.push({ key: key, name: data[key] });
       }
       this.setState({
         test: newState
       });
-      // console.log(response.val());
     });
   }
 
   render() {
     return (
       <main>
-        <div className = "wrapper">
-        
-        <Search />
-        <div className="events">
-          {this.state.isLoading ? (
-            <p>...Loading</p>
-          ) : (
-            this.state.filteredEvents.map(event => {
-              return (
-                <div className="eventContainer" key={event.id}>
-                  <h3>Title: {event.name}</h3>
-                  <p>Start Date: {event.dates.start.localDate}</p>
-                  <p>Info: {event.info}</p>
-                  {event.priceRanges[0].min === event.priceRanges[0].max ? (
-                    <p>the price: {event.priceRanges[0].min}</p>
-                  ) : (
+        <div className="wrapper">
+          <Search
+            value={this.state.search}
+            searchInput={this.searchInput}
+            searchSubmit={this.searchSubmit}
+          />
+          <div className="events">
+            {this.state.isLoading ? (
+              <p>...Loading</p>
+            ) : (
+              this.state.filteredEvents.map(event => {
+                return (
+                  <div className="eventContainer" key={event.id}>
+                    <h3>Title: {event.name}</h3>
+                    <p>Start Date: {event.dates.start.localDate}</p>
+                    <p>Info: {event.info}</p>
+                    {event.priceRanges[0].min === event.priceRanges[0].max ? (
+                      <p>the price: {event.priceRanges[0].min}</p>
+                    ) : (
+                      <p>
+                        The price range: {event.priceRanges[0].min} -{" "}
+                        {event.priceRanges[0].max}
+                      </p>
+                    )}
                     <p>
-                      The price range: {event.priceRanges[0].min} -{" "}
-                      {event.priceRanges[0].max}
+                      <a href={event.url}>TicketMaster Link</a>
                     </p>
-                  )}
-                  <p>
-                    <a href={event.url}>TicketMaster Link</a>
-                  </p>
-                  <img src={event.images[0].url} alt={event.name} />
-                  
-                  <div>
-                    {this.state.test.map((testItem) => {
-                      return (
-                        <li key={testItem.key}>
-                          {/* <p>{testItem.name} - {testItem.key}</p>
+                    <img src={event.images[2].url} alt={event.name} />
+
+                    <div>
+                      {this.state.test.map(testItem => {
+                        return (
+                          <li key={testItem.key}>
+                            {/* <p>{testItem.name} - {testItem.key}</p>
             </li>
             <li key={i}> */}
-                          <p>{testItem.name}</p>
-                          <button onClick={() => this.removeTestItem(testItem.key)}> Remove Item </button>
-                        </li>)
-                    })}
-                    <form action="submit">
-                      <label htmlFor="newTestItem">Add a test item to your test</label>
-                      <input
-                        type="text"
-                        id="newItem"
-                        onChange={this.handleChange}
-                        value={this.state.userInput} />
+                            <p>{testItem.name}</p>
+                            <button
+                              onClick={() => this.removeTestItem(testItem.key)}
+                            >
+                              {" "}
+                              Remove Item{" "}
+                            </button>
+                          </li>
+                        );
+                      })}
+                      <form action="submit">
+                        <label htmlFor="newTestItem">
+                          Add a test item to your test
+                        </label>
+                        <input
+                          type="text"
+                          id="newItem"
+                          onChange={this.handleChange}
+                          value={this.state.userInput}
+                        />
 
-                      <button onClick={this.handleClick}>Add Test Item</button>
-
-                    </form>
+                        <button onClick={this.handleClick}>
+                          Add Test Item
+                        </button>
+                      </form>
+                    </div>
                   </div>
-
-
-
-                </div>
-              );
-            })
-          )}
-        </div>
+                );
+              })
+            )}
+          </div>
         </div>
       </main>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 
 class Search extends Component {
-    render(){
-        return(
-            <div className="search">
-                <h2>Search Events</h2>
-                <label htmlFor="userSearch"></label>
-                <input type="text" id="userSearch" placeholder="Search Event, Show, Artist..." className="userSearch"></input>
-                <button className="" type="submit" value="Submit">Find Events</button>
-        
-                {/* <h2>`{this.state.userName}'s Results`</h2>  */}
-                <h3 className="searchResults">Search Results</h3>
-            </div>
-        )
-    }
+  render() {
+    const { value, searchInput, searchSubmit } = this.props;
+    return (
+      <div className="search">
+        <h2>Search Events</h2>
+        <form className="searchBar" onSubmit={searchSubmit}>
+          <label htmlFor="userSearch" className="visuallyHidden">
+            Search Events
+          </label>
+          <input
+            type="text"
+            id="userSearch"
+            name="userSearch"
+            placeholder="Search Event, Show, Artist..."
+            value={value}
+            onChange={searchInput}
+          />
+          <button className="searchButton" type="submit" value="Submit">
+            Find Events
+          </button>
+        </form>
+
+        {/* <h2>`{this.state.userName}'s Results`</h2>  */}
+        <h3 className="searchResults">Search Results</h3>
+      </div>
+    );
+  }
 }
 
 export default Search;
